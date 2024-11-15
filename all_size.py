832d00252cfbe782d22ee420ad109fe1b1b4c1ce
Yang Yu <52021992+yuyang-0825@users.noreply.github.com>
2024-11-15 14:27:04 -0600

    Update all_size.py

--- all_size.py
+++ all_size.py
@@ -240,7 +240,7 @@ def enumerate_triangle(graph_path,model_path,labelnum,k):
 
     max_occur = 0
     max_pattern = initial_pattern.copy()
-    for labels in tqdm(combinations, desc="Enumrateing size-3 CC motifs"):
+    for labels in tqdm(combinations, desc="Enumrating CC motifs"):
         initial_pattern.vs["label"] = labels
         initial_pattern.es["label"] = 0
         initial_pattern["type"] = labels
