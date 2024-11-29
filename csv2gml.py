be588046d443ea77e0c7946ed1b2181e953cd3d9
Yang Yu <52021992+yuyang-0825@users.noreply.github.com>
2024-11-28 20:12:19 -0600

    Update csv2gml.py

--- csv2gml.py
+++ csv2gml.py
@@ -7,14 +7,17 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Transfer input to gml file')
-    parser.add_argument('--path', type=str, default='demo_data/demo_data.csv',
-                        help='The path of input data')
+    parser.add_argument('--graph', type=str, default='demo_data/demo_data.csv',
+                        help='The path of input graph data')
+    parser.add_argument('--motif_size', type=int, default=3,
+                        help='The size of input motif')
+    parser.add_argument('--motif_label', type=str, default="Micro&Micro&Micro" ,help='The cell type of input motif(combine with "&")')
     args = parser.parse_args()
     return args
 
 if __name__ == '__main__':
     args = parse_args()
-    input_path = args.path
+    input_path = args.graph
     input_folder = input_path.split('/')[0]
     input_name = input_path.split('/')[1]
 
@@ -47,4 +50,32 @@ if __name__ == '__main__':
     graph.es["label"] = 0
 
     graph.write(os.path.join(input_folder, input_name.split('.')[0] + '.gml') )
+
+    if args.motif_size:
+        motif_size = args.motif_size
+        motif_label = args.motif_label
+
+        if motif_size == 3:
+            motif = Graph(n=3, edges=[[0, 1], [0, 2], [1, 2]])
+        elif motif_size == 4:
+            motif = Graph(n=4, edges=[[0, 1], [0, 2], [1, 2], [1, 3], [2, 3]])
+        elif motif_size == 5:
+            motif = Graph(n=5, edges=[[0, 1], [0, 2], [1, 2], [0, 3], [0, 4], [3, 4]])
+        elif motif_size == 6:
+            motif = Graph(n=6, edges=[[0, 1], [0, 2], [1, 2], [2, 3], [2, 4], [3, 4], [4, 5], [3, 5]])
+        elif motif_size == 7:
+            motif = Graph(n=7, edges=[[0, 1], [0, 2], [1, 2], [2, 3], [2, 4], [3, 4], [3, 5], [3, 6], [5, 6]])
+        elif motif_size == 8:
+            motif = Graph(n=8, edges=[[0, 1], [0, 2], [1, 2], [2, 3], [2, 4], [3, 4], [3, 5], [3, 6], [5, 6],
+                                                [5, 7], [6, 7]])
+        elif motif_size == 9:
+            motif = Graph(n=9, edges=[[0, 1], [0, 2], [1, 2], [2, 3], [2, 4], [3, 4], [3, 5], [3, 6], [6, 7],
+                                                [6, 8], [7, 8]])
+        motif_label = motif_label.split('&')
+        motif_label_int = [label_to_int[label] for label in motif_label]
+        motif.vs["label"] = motif_label_int
+        motif.es["label"] = 0
+
+        motif.write(os.path.join(input_folder, 'size-'+str(motif_size)+'.gml'))
+
 
