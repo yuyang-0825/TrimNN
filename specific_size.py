353e955ed4aa15ea355ea9f806351b41408abd32
Yang Yu <52021992+yuyang-0825@users.noreply.github.com>
2024-12-02 19:01:49 -0600

    Update specific_size.py

--- specific_size.py
+++ specific_size.py
@@ -277,7 +277,7 @@ def parse_args():
                         help='specific size of CC motif (from 3 to 9)')
     parser.add_argument('-k', type=int, default=2,
                         help='k-hop')
-    parser.add_argument('-target', type=str, default='spatial_data/demo_data.gml',
+    parser.add_argument('-target', type=str, default='demo_data/demo_data.gml',
                         help='path of input target cellular community graph')
     parser.add_argument('-celltype', type=int, default=8,
                         help='number of cell types')
